9ab0066da59aa045fec8301357fa53f19099cbe6
LaCreArthur <le-tutur@hotmail.fr>
2025-02-27 10:50:07 +0100

    feat: add EnemySO scriptable object for enemy configuration

---
+++ Assets/Scripts/Enemies/EnemySO.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy/EnemySO")]
+public class EnemySO : ScriptableObject
+{
+    public GameObject prefab;
+    public float maxHealth;
+    public float moveSpeed;
+    public int goldReward;
+    public int damageToPlayer;
+}
--- Assets/Scripts/Tower/EnemyHealthSystem.cs
+++ Assets/Scripts/Tower/EnemyHealthSystem.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 
 public class EnemyHealthSystem : HealthSystem, IPoolable
 {
+    [SerializeField] EnemySO enemySO;
     public GameObject LastBullet { get; set; } // Add this
-
+    void Awake() => MaxHp = enemySO.maxHealth;
     public void OnSpawn() => CurrentHp = MaxHp;
 
     public override void TakeDamage(int amount)
