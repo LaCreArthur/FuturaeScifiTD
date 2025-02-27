743f0fc20b3ab847278ad5d1aa941a23c2fa0565
LaCreArthur <le-tutur@hotmail.fr>
2025-02-27 11:21:35 +0100

    feat: improved spawner for enemy waves (basic)

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] EnemySO[] enemyTypes;
+    [SerializeField] int waveSize = 10;
     [SerializeField] float spawnRate = 1f;
 
+    int _waveNumber;
+    int _waveEnemyCount;
     float _timeUntilNextSpawn;
 
     void Awake() => PathGenerator.PathGenerated += OnPathGenerated;
@@ -16,8 +19,19 @@ public class EnemySpawner : MonoBehaviour
         _timeUntilNextSpawn -= Time.deltaTime;
         if (_timeUntilNextSpawn <= 0)
         {
-            PoolManager.Spawn(enemyPrefab, transform.position, Quaternion.identity);
+            SpawnEnemy();
             _timeUntilNextSpawn = spawnRate;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        PoolManager.Spawn(enemyTypes[_waveNumber].prefab, transform.position, Quaternion.identity);
+        _waveEnemyCount++;
+        if (_waveEnemyCount >= waveSize)
+        {
+            _waveNumber = (_waveNumber + 1) % enemyTypes.Length;
+            _waveEnemyCount = 0;
         }
     }
 
